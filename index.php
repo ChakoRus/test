4f95f8a53a7fe7d16a55481e5c59ad89b8e72639
ChakoRus <chalenkoa@yandex.ru>
2024-05-25 13:26:08 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
 <?php
 
-class Test {
+class Test
+{
+	private $name;
+
+	public function __construct($name)
+	{
+		$this->name = $name;
+	}
 
 }
 
-var_dump(new Test());
+var_dump(new Test('Max'));
